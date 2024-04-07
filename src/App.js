586408f19cccedaad790a7312b6bed6f1a66937e
Taylor Maryon <50721884+tmaryon@users.noreply.github.com>
2024-04-07 10:54:31 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+  async function getAdvice() {
+      const res = await fetch('https://api.adviceslip.com/advice');
+      const data = await res.json();
+      setAdvice(data.slip.advice); 
+      setCount((c) => c + 1);
+  }
+
+  useEffect(function() {
+    getAdvice();
+  }, [])
+
+  return (
+    <div>
+      <h1>{ advice }</h1>
+      <button onClick={ getAdvice }>Get advice</button>
+      <p>You have sought advice: <strong>{ count }</strong> times.</p>
+    </div>
+  );
+}
